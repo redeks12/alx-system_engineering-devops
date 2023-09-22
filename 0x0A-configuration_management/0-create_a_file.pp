b7a1d8d2057a074c4cda8de1016135344dd767f6
kumdan job <redeks123456@gmail.com>
2023-09-22 20:45:23 +0100

    configurations management task 1

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Using Puppet create a file in /tmp.
 
-file { 'school':
-  path    => '/tmp/school',
-  mode    => '0744',
+file { '/tmp/school':
+  ensure  => present,
+  content => 'I love Puppet',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+  mode    => '0744',
 }
