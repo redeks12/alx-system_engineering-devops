1ef25e953e90630d94cb457d0121b4a9e63b99a3
kumdan job <redeks123456@gmail.com>
2023-09-23 13:54:51 -0700

    advanced task

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# using puppet to configure ssh
+
+file { 'etc/ssh/ssh_config':
+	ensure  => present,
+	content => '
+	#ssh client configuration
+	host *
+		IdentityFile ~/.ssh/school
+		PasswordAuthentication no'
+}
