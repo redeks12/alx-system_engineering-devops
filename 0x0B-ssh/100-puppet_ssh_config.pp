994fddd2b0d6b61fdb53e3ac24cb8d705f3f9e4a
kumdan job <redeks123456@gmail.com>
2023-09-24 23:31:54 +0100

    m

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,17 +3,19 @@
 include stdlib
 
 file_line { 'add_idetity_file':
-	path  => '/etc/ssh/ssh_config',
-	line  => '		IdentityFile ~/.ssh/school',
-	match => '		# IdentityFile ~/.ssh/school',
-
-	PasswordAuthentication no
+	path  			   => '/etc/ssh/ssh_config',
+	line               => '		IdentityFile ~/.ssh/school',
+	match              => '		# IdentityFile ~/.ssh/school',
+	replace            => True,
+	append_on_no_match => True,
 }
 
 file_line { 'no_auth':
-	path  => '/etc/ssh/ssh_config',
-	line  => '		PasswordAuthentication no',
-	match => '		PasswordAuthentication yes',
+	path  			   => '/etc/ssh/ssh_config',
+	line               => '		PasswordAuthentication no',
+	match              => '		PasswordAuthentication yes',
+	replace            => True,
+	append_on_no_match => True,
 
 	
 }
