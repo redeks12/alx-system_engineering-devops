b0d94b79a254c97459b2e2552a9f7966affa58a1
kumdan job <redeks123456@gmail.com>
2023-09-24 23:28:05 +0100

    m

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 # using puppet to configure ssh
+import stdlib
 
-file { 'etc/ssh/ssh_config':
-	ensure => present,
+file_line { 'add_idetity_file':
+	path  => '/etc/ssh/ssh_config',
+	line  => '		IdentityFile ~/.ssh/school',
+	match => '		# IdentityFile ~/.ssh/school',
 
-content =>"
-
-	#ssh client configuration
-	host*
-	IdentityFile ~/.ssh/school
 	PasswordAuthentication no
-	",
+}
+
+file_line { 'no_auth':
+	path  => '/etc/ssh/ssh_config',
+	line  => '		PasswordAuthentication no',
+	match => '		PasswordAuthentication yes',
+
+	
 }
