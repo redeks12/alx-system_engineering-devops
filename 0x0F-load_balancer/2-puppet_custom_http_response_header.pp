0e290da6fa39020c98cef2930f2d761298bc5a95
kumdan job <redeks123456@gmail.com>
2023-10-03 00:51:11 +0100

    m

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,23 +1,22 @@
 # Automating project requirements using Puppet
 
-# package { 'nginx':
-#   ensure => installed,
-# }
+package { 'nginx':
+  ensure => installed,
+}
 $host_name = $::hostname
-notify { "PATH Variable": message => "PATH is $hostname" }
 
-# file_line { 'install':
-#   ensure => 'present',
-#   path   => '/etc/nginx/sites-enabled/default',
-#   after  => 'server_name _;',
-#   line   => 'add_header X-Served-By $HOSTNAME;',
-# }
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'server_name _;',
+  line   => 'add_header X-Served-By $host_name;',
+}
 
-# file { '/var/www/html/index.html':
-#   content => 'Hello World!',
-# }
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
 
-# service { 'nginx':
-#   ensure  => running,
-#   require => Package['nginx'],
-# }
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
