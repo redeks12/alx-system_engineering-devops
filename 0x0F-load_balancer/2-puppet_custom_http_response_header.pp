62a6e16f97407b3ef1ad86d50023058cc47cad78
kumdan job <redeks123456@gmail.com>
2023-10-03 01:00:57 +0100

    m

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,13 +1,12 @@
 # Automating project requirements using Puppet
 
-exec { add_header:
-    command =>"sudo apt-get update
-    sudo apt-get -y install nginx
-    sudo ufw allow 'Nginx HTTP'
-    sudo sed -i 's|server_name _;|server_name _;\n\tadd_header X-Served-By $HOSTNAME;|' /etc/nginx/sites-enabled/default
-    sudo nginx -t
-    sudo service nginx restart",
+exec { 'add_header':
+  command =>"sudo apt-get update;
+  sudo apt-get -y install nginx;
+  sudo ufw allow 'Nginx HTTP'
+  sudo sed -i 's|server_name _;|server_name _;\n\tadd_header X-Served-By $HOSTNAME;|' /etc/nginx/sites-enabled/default
+  sudo service nginx restart",
 
-    provider => "shell"
+  provider => 'shell',
 
 }
