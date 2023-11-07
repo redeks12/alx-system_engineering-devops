6029739b64f7c1c4732dae955dd76a01f35be602
kumdan job <redeks123456@gmail.com>
2023-11-07 22:36:20 +0100

    m

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Write a function that queries the
+Reddit API and prints the titles of the first
+10 hot posts listed for a given subreddit."""
+
+import requests
+
+
+def count_words(subreddit, word_list, hot_list=[], after=None):
+    """Write a function that queries the
+    Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit."""
+    my_list = hot_list
+    params = {"limit": "100"}
+    if after:
+        params["after"] = after
+    response = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={
+            "User-Agent": "subscribercheck",
+        },
+        params=params,
+        allow_redirects=False,
+    )
+    if response.status_code is not 200:
+        return None
+    resp = response.json()["data"]["children"]
+    if resp:
+        for i in resp:
+            my_list.append(i["data"]["title"])
+
+        afterd = response.json()["data"]["after"]
+        if afterd is None:
+            word_dict = {val: 0 for val in word_list}
+            for item in my_list:
+                new_str = item.lower().split()
+                for key in new_str:
+                    if key in word_dict:
+                        word_dict[key] += 1
+            sort_dict = sorted(word_dict.items(), key=lambda item: (-item[1], item[0]))
+            [print("{}: {}".format(key, val)) for key, val in sort_dict if val]
+            return my_list
+        else:
+            return count_words(subreddit, word_list, my_list, afterd)
